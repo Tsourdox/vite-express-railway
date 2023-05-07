78190cb507fa7328f7a2bb20d4c3865e90f5f1ea
David Jensen <tsourdox@icloud.com>
2023-05-07 13:32:36 +0200

    add cors to backend

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const port = process.env.PORT || 3000;
 
 const app = express();
+app.use(cors({ url: 'vite-express-frontend.up.railway.app' }));
 
 app.get('/', (req, res) => {
   res.json('Hello World!');
